626b51e2832df1e36c2be761b46e94baa3ee2100
Sijmen J. Mulder <ik@sjmulder.nl>
2022-01-30 21:26:32 +0000

    Comments

--- fizzbuzz.s
+++ fizzbuzz.s
@@ -16,16 +16,16 @@ main:
 	mov r6, #5	// Buzz countdown
 	mov r7, #15	// FizzBuzz countdown
 _loop:
-	ldr   r0, =nfmt
-	subs  r5, #1
-	moveq r5, #3
-	ldreq r0, =fizz
-	subs  r6, #1
-	moveq r6, #5
-	ldreq r0, =buzz
-	subs  r7, #1
-	moveq r7, #15
-	ldreq r0, =fibu
+	ldr   r0, =nfmt	// fmt = '%d'
+	subs  r5, #1	// Fizz -= 1
+	moveq r5, #3	//  if 0, Fizz = 3
+	ldreq r0, =fizz	//  if 0, fmt = 'fizz'
+	subs  r6, #1	// Buzz -= 1
+	moveq r6, #5	//  if 0, Fizz = 5
+	ldreq r0, =buzz //  if 0, fmt = 'Buzz'
+	subs  r7, #1	// FizzBuzz -= 1
+	moveq r7, #15	//  if 0, FizzBuzz = 1
+	ldreq r0, =fibu	//  if 0, fmt = 'FizzBuzz'
 	mov   r1, r4
 	bl    printf
 	add   r4, 1
